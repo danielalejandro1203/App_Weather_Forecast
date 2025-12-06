1704aa84eba9d65a8252389f66c53f6d93db18dd
Daniel <d.alejandro1203@gmail.com>
2025-12-05 22:04:24 -0400

    Day 34: Toques finales

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ import plotly.express as px
 from backend import get_data
 
 # Add title, text input, slider, selectbox, and subheader
-st.title("Weather Forecast for the next Days")
-place = st.text_input("Place: ")
+st.title("Weather Forecast for the Next Days")
+place = st.text_input("Place:")
 days = st.slider("Forecast Days", min_value=1, max_value=5,
                  help="Select the number of forecasted days")
 option = st.selectbox("Select data to view",
@@ -12,29 +12,40 @@ option = st.selectbox("Select data to view",
 
 st.subheader(f"{option} for the next {days} days in {place}")
 if place:
-
-    # Get the temperature/sky data
     try:
+        # Get the temperature/sky data
         filtered_data = get_data(place, days)
 
         if option == "Temperature":
-            temperatures = [dict["main"]["temp"] / 10 for dict in filtered_data]
-            dates = [dict["dt_txt"] for dict in filtered_data]
-            # Create a temperature plot
-            figure = px.line(x= dates, y=temperatures, labels={"x": "Date", "Temperature": "Temperature (C)"})
+            temperatures = [d["main"]["temp"] for d in filtered_data]
+            dates = [d["dt_txt"] for d in filtered_data]
+
+            figure = px.line(
+                x=dates,
+                y=temperatures,
+                labels={"x": "Date", "y": "Temperature (°C)"}
+            )
             st.plotly_chart(figure)
 
+
         if option == "Sky":
-            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
-                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
-            sky_conditions = [dict["weather"][0]["main"] for dict in filtered_data]
-            image_paths = [images[condition] for condition in sky_conditions]
-            print(sky_conditions)
-            st.image(image_paths, width=115)
+            images = {
+                "Clear": "images/clear.png",
+                "Clouds": "images/cloud.png",
+                "Rain": "images/rain.png",
+                "Snow": "images/snow.png"
+            }
+
+            sky_conditions = [d["weather"][0]["main"] for d in filtered_data]
+            dates = [d["dt_txt"] for d in filtered_data]
+            image_paths = [images[c] for c in sky_conditions]
+
+            cols = st.columns(4)
+
+            for i in range(len(image_paths)):
+                with cols[i % 4]:
+                    st.image(image_paths[i], width=80)
+                    st.write(dates[i])
+
     except KeyError:
-        st.write("That place does not exist.")
-
-
-
-
-
+        st.error("That place does not exist. Please try again.")
